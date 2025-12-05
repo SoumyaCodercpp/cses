3ce72b133bf3d87c5eb950258293cd07cacaddff
SoumyaCodercpp <soumyajitpal2803@gmail.com>
2025-12-06 02:33:23 +0530

    5/12/25

---
+++ projects.cpp
@@ -0,0 +1,142 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define INF (long long)1e18 + 5 //Infinity
+#define MOD 1000000007
+// #define M_PI 3.141592653589793
+#define f(j,n,k) for(int i=j;i<n;i=i+k)
+#define read(a) for(auto &i: a) cin >> i
+#define print(a) for(auto &i: a) cout << i << " "; cout << "\n"
+#define read2(a, n, m) for (int i = 0; i < (n); ++i) for (int j = 0; j < (m); ++j) cin >> (a)[i][j]
+#define print2(a, n, m) for (int i = 0; i < (n); ++i) { for (int j = 0; j < (m); ++j) cout << (a)[i][j] << " "; cout << endl; }
+#define int2(n,k) int n, k; cin >> n >> k
+#define int3(n,x,y) int n, x, y; cin >> n >> x >> y
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define ss second
+#define ff first
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define sz(a) (ll)a.size()
+#define all(a) a.begin(), a.end()
+#define tri pair<ll, pll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vlll vector<tri>
+#define vvi vector<vi>
+#define vvl vector<vl>
+// #define clear(arr) memset(arr, 0, sizeof (arr))
+#define YES cout<<"yes\n"
+#define NO cout<<"no\n"
+#define modulo 998244353 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int a){
+    cerr<<a;
+}
+void _print(long long a){
+    cerr<<a;
+}
+void _print(bool a){
+    cerr<<a;
+}
+void _print(string a){
+    cerr<<a;
+}
+void _print(char a){
+    cerr<<a;
+}
+template<class T> void _print(vector<T>v){
+    cerr<<"[ ";
+    for(T i:v){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+template<class T> void _print(set<T>s){
+    cerr<<"[ ";
+    for(T i:s){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+ 
+ 
+ll fact2(ll n){
+    return (n*(n-1))/2;
+}
+ 
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll n;
+    cin>>n;
+    ll t=n;
+    vll p;
+    vl pre(n);
+    map<pair<ll,ll>,ll>m;
+    map<pair<ll,ll>,bool>vis;
+    while(t--){
+ 
+        ll a,b,c;
+        cin>>a>>b>>c;
+        p.pb({b,a});
+        if(vis[{b,a}]){
+ 
+            m[{b,a}]=max(m[{b,a}],c);
+ 
+        }
+        else{
+            vis[{b,a}]=true;
+            m[{b,a}]=c;
+        }
+ 
+ 
+ 
+    }
+    sort(all(p));
+    vl f1(n);
+   
+    f1[0]=p[0].ff;
+    f(1,n,1){
+        
+        f1[i]=p[i].ff;
+    }
+    vl dp(n);
+ 
+    dp[0]=m[p[0]];
+    f(1,n,1){
+        dp[i]=dp[i-1];
+        ll j=lower_bound(all(f1),p[i].ss)-f1.begin();
+        if(j==0){
+            dp[i]=max(dp[i],m[p[i]]);
+        }
+        else{
+ 
+            dp[i]=max(dp[i],dp[j-1]+m[p[i]]);
+ 
+        }
+    }
+ 
+    debug(dp)
+ 
+    cout<<dp[n-1]<<endl;
+ 
+ 
+ 
+ 
+ 
