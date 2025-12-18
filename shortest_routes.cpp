c544f6ce7217cedd2adc8f33c2297c1c7a2dca28
SoumyaCodercpp <soumyajitpal2803@gmail.com>
2025-12-18 22:48:16 +0530

    16/12/25

---
+++ shortest_routes.cpp
@@ -0,0 +1,158 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define INF (long long)1e18 + 5 //Infinity
+#define MOD 1000000007
+// #define M_PI 3.141592653589793
+#define f(j,n,k) for(int i=j;i<n;i=i+k)
+#define read(a) for(auto &i: a) cin >> i
+#define print(a) for(auto &i: a) cout << i << " "; cout << "\n"
+#define read2(a, n, m) for (int i = 0; i < (n); ++i) for (int j = 0; j < (m); ++j) cin >> (a)[i][j]
+#define print2(a, n, m) for (int i = 0; i < (n); ++i) { for (int j = 0; j < (m); ++j) cout << (a)[i][j] << " "; cout << endl; }
+#define int2(n,k) int n, k; cin >> n >> k
+#define int3(n,x,y) int n, x, y; cin >> n >> x >> y
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define ss second
+#define ff first
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define sz(a) (ll)a.size()
+#define all(a) a.begin(), a.end()
+#define tri pair<ll, pll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vlll vector<tri>
+#define vvi vector<vi>
+#define vvl vector<vl>
+// #define clear(arr) memset(arr, 0, sizeof (arr))
+#define YES cout<<"yes\n"
+#define NO cout<<"no\n"
+#define modulo 998244353 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int a){
+    cerr<<a;
+}
+void _print(long long a){
+    cerr<<a;
+}
+void _print(bool a){
+    cerr<<a;
+}
+void _print(string a){
+    cerr<<a;
+}
+void _print(char a){
+    cerr<<a;
+}
+template<class T> void _print(vector<T>v){
+    cerr<<"[ ";
+    for(T i:v){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+template<class T> void _print(set<T>s){
+    cerr<<"[ ";
+    for(T i:s){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+ 
+ 
+ll fact2(ll n){
+    return (n*(n-1))/2;
+}
+ 
+ 
+ ll fact(ll o, ll m){
+    if(o==0||o==1){
+        return 1;
+    }
+    return (o*fact(o-1,m))%m;
+}
+ 
+ 
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+ 
+ 
+        
+ 
+   
+ 
+   ll n,m;
+   cin>>n>>m;
+ 
+   vector<vll>adj(n+1);
+ 
+   
+ 
+   f(0,m,1){
+    ll l,r,c;
+    cin>>l>>r>>c;
+    adj[l].pb({c,r});
+    
+   }
+ 
+   vl ans(n+1,LLONG_MAX);
+ 
+   set<pll>s;
+   s.insert({0,1});
+   ans[1]=0;
+   while(!s.empty()){
+ 
+    auto t=*(s.begin());
+    ll d=t.ff;
+    ll e=t.ss;
+    s.erase(s.begin());
+    for(auto j:adj[e]){
+        ll w=j.ff;
+        ll m=j.ss;
+        if(ans[m]>(w+d)){
+            
+            auto r=s.find({ans[m],m});
+            if(r!=s.end()) s.erase(r);
+            ans[m]=d+w;
+            s.insert({ans[m],m});
+        }
+    }
+ 
+ 
+ 
+   }
+ 
+   f(1,n+1,1){
+    cout<<ans[i]<<" ";
+   }
+   cout<<endl;
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
---
+++ shortest_routes_2.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define INF (long long)1e18 + 5 //Infinity
+#define MOD 1000000007
+// #define M_PI 3.141592653589793
+#define f(j,n,k) for(int i=j;i<n;i=i+k)
+#define read(a) for(auto &i: a) cin >> i
+#define print(a) for(auto &i: a) cout << i << " "; cout << "\n"
+#define read2(a, n, m) for (int i = 0; i < (n); ++i) for (int j = 0; j < (m); ++j) cin >> (a)[i][j]
+#define print2(a, n, m) for (int i = 0; i < (n); ++i) { for (int j = 0; j < (m); ++j) cout << (a)[i][j] << " "; cout << endl; }
+#define int2(n,k) int n, k; cin >> n >> k
+#define int3(n,x,y) int n, x, y; cin >> n >> x >> y
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define ss second
+#define ff first
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define sz(a) (ll)a.size()
+#define all(a) a.begin(), a.end()
+#define tri pair<ll, pll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vlll vector<tri>
+#define vvi vector<vi>
+#define vvl vector<vl>
+// #define clear(arr) memset(arr, 0, sizeof (arr))
+#define YES cout<<"yes\n"
+#define NO cout<<"no\n"
+#define modulo 998244353 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int a){
+    cerr<<a;
+}
+void _print(long long a){
+    cerr<<a;
+}
+void _print(bool a){
+    cerr<<a;
+}
+void _print(string a){
+    cerr<<a;
+}
+void _print(char a){
+    cerr<<a;
+}
+template<class T> void _print(vector<T>v){
+    cerr<<"[ ";
+    for(T i:v){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+template<class T> void _print(set<T>s){
+    cerr<<"[ ";
+    for(T i:s){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+ 
+ 
+ll fact2(ll n){
+    return (n*(n-1))/2;
+}
+ 
+ 
+ ll fact(ll o, ll m){
+    if(o==0||o==1){
+        return 1;
+    }
+    return (o*fact(o-1,m))%m;
+}
+ 
+ 
+ 
+ 
+ 
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+char dir[4]={'D','U','R','L'};
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+ll n,m,q;
+cin>>n>>m>>q;
+ 
+ 
+// vector<vector<pll>>adj(n+1);
+vvl prev(n+1,vl(n+1,INF));
+ 
+f(1,n+1,1){
+    prev[i][i]=0;
+}
+f(0,m,1){
+    ll a,b,c;
+    cin>>a>>b>>c;
+    prev[a][b]=min(prev[a][b],c);
+    prev[b][a]=min(prev[b][a],c);
+    
+}
+ 
+for(int k=1;k<=n;k++){
+   
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            
+                prev[i][j]=min(prev[i][j],prev[i][k]+prev[k][j]);
+            
+        }
+    }
+    
+ 
+}
+ 
+f(0,q,1){
+    ll a,b;
+    cin>>a>>b;
+    if(prev[a][b]>=INF) cout<<-1<<endl;
+    else cout<<prev[a][b]<<endl;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+}
